98fec2e9ea03ee4534f349ede54c1785f08274f4
iamsanamrai <lonelykan6a@gmail.com>
2023-12-17 21:03:44 +0530

    added javascript code for the text animation

--- myscript.js
+++ myscript.js
@@ -2,3 +2,18 @@ function scrollToSection(id){
     var element = document.getElementById(id);
     element.scrollIntoView({behavior: 'smooth'});
 }
+
+var i = 0;
+var txt = 'Frontend Developer and Cloud Engineer';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+window.onload = function() {
+  typeWriter();
+};
